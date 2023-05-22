99bcbf19e0baec39d2f20b4a742788e3e1c37472
YanAntunes <114197358+YanAntunes@users.noreply.github.com>
2023-05-22 20:01:53 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log ('hello "world!"'); // string = texto
+console.log ("hello 'world'"); // string
+console.log (`"hello" 'world'`); // string
+
+console.log (35); //number
+console.log (15.85); //number
+
+console.log ('Meu nome é "YAN ANTUNES". Estou aprendendo JavaScript às 20 horas da noite') 
+
+console.log ('Meu nome é "YAN ANTUNES". Estou aprendendo JavaScript às', 20, 'horas da noite')
